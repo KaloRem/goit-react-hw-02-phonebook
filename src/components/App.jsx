ac1b957a88a0b235dd898dfc06a11a9697b78b6e
KaloRem <bogdan.paslawskii@gmail.com>
2023-10-29 18:51:20 +0100

    v1.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,8 @@ import { SearchFilter } from './SearchFilter/SearchFilter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    contacts: [],
+    filter: "",
   };
 
   addContact = newContact => {
